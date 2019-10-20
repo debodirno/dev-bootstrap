620e31054a77b511a7b3b30b3a5b909fa33da204
ikad95 <moulik.adak@gmail.com>
2019-10-20 13:09:00 +0530

    A script to put your password on clipboard which can be triggered by a shortcut

---
+++ misc/password-to-clipboard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Map a shortcut like Alt + P to this script.
+# This script will put your password on your clipboard.
+# Use Ctrl + V to paste your password anywhere.
+
+
+echo "YOUR_PASSWORD" | xclip -sel clip
