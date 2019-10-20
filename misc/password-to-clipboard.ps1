1c4293a92bf516363bd7bcec109a7656ec120810
psaswata07 <56783019+psaswata07@users.noreply.github.com>
2019-10-20 14:59:19 +0530

    Create password-to-clipboard.ps1

---
+++ misc/password-to-clipboard.ps1
@@ -0,0 +1,5 @@
+# Map a shortcut like Alt + P to this script.
+# This script will put your password on your clipboard.
+# Use Ctrl + V to paste your password anywhere.
+
+echo "YOUR_PASSWORD" | Set-Clipboard
